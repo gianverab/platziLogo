8f9db5a34263e53378b09e67acc73d2bf1fbe2e0
Giancarlo Vera Burgos <Giancarlo Vera Burgos>
2017-01-08 23:39:06 -0300

    drawing a Platzi logo

---
+++ main.js
@@ -0,0 +1,23 @@
+var box = document.getElementById('myCanvas');
+var lienzo = box.getContext('2d');
+var green = '#64dd17';
+
+function drawPlatzi(color, stroke){
+	lienzo.beginPath();
+	lienzo.strokeStyle = color;
+	lienzo.lineWidth = stroke;
+	lienzo.moveTo(280,320);
+	lienzo.lineTo(380,220);
+	lienzo.arcTo(400,200,380,180,30);
+	lienzo.lineTo(270,70);
+	lienzo.arcTo(250,50,230,70,30);
+	lienzo.lineTo(70,230);
+	lienzo.arcTo(50,250,70,270,30);
+	lienzo.lineTo(220,420);
+	lienzo.arcTo(235,435,260,410,30);
+	lienzo.lineTo(315,355);
+	lienzo.stroke();
+	lienzo.closePath();
+}
+
+drawPlatzi(green,50);
